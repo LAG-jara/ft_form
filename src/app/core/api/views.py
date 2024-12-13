65b8aa1b6583d7d0ceef5c2b031032e4433e55bd
Nadezhda Zhuzhleva <nzhuzhle@car9s6.42barcelona.com>
2024-12-13 12:09:04 +0100

    fixed users right

--- src/app/core/api/views.py
+++ src/app/core/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.exceptions import NotFound
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.exceptions import PermissionDenied
 from rest_framework import status
 import logging
